21cfe393b4789d098a3b35d56c831b7e37d6b56f
Erez Cohen <erezcohen@shenkar508-9.tau.ac.il>
2016-11-09 15:48:26 +0200

    Wed Nov  9 15:48:26 IST 2016

--- AnaTreesPackage/mac/muon_proton_id.py
+++ AnaTreesPackage/mac/muon_proton_id.py
@@ -73,7 +73,8 @@ if flags.option=="find common muon-proton vertices" or 'find_mu_p' in flags.opti
     print_important( "find common muon-proton vertices" )
     SchemedResultFileName = schemed_anatrees_file_name( "GOOD"+flags.DataType+"_filesana.list" , Sel2muons_intersection_list_name( GBDTmodelName ,TracksListName , p_score ) )
     IntersectionListName = mu_p_intersection_path + "/" + Sel2muons_intersection_list_csv_name( GBDTmodelName , TracksListName , p_score )
-    extract_anatrees_tracks_information_from_a_file( SchemedResultFileName , "find common muon-proton vertices" ,
+    extract_anatrees_tracks_information_from_a_file( flags.DataType,
+                                                    SchemedResultFileName , "find common muon-proton vertices" ,
                                                     TreeName="anatree" ,
                                                     AddEventsList=True ,
                                                     EventsListName=IntersectionListName)
