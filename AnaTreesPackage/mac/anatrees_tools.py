b99ff54ccb682d30a6085c55f26e60a50f734c09
Erez Cohen <erezcohen@shenkar508-9.tau.ac.il>
2016-11-15 09:25:40 +0200

    Tue Nov 15 09:25:40 IST 2016

--- AnaTreesPackage/mac/anatrees_tools.py
+++ AnaTreesPackage/mac/anatrees_tools.py
@@ -85,7 +85,7 @@ def read_files_from_a_list( ListName , first_anatree_file , last_anatree_file ):
     if flags.verbose: print_filename(ListName,"reading list of files (collecting files %d to %d)..."%(first_anatree_file , last_anatree_file))
     with open( lists_path + "/analysis_trees/" + ListName + ".list") as f:
         files = f.read().splitlines()
-        files = files[ first_anatree_file : last_anatree_file ]
+    files = files[ first_anatree_file : last_anatree_file ]
     if flags.verbose>4: print files
     return files
 
