e690b6389bea366537d68d3444a1cb25dab13540
Erez Cohen <cohen.erez7@gmail.com>
2016-12-08 09:17:27 -0800

    Thu Dec  8 09:17:27 PST 2016

--- AnaTreesPackage/mac/anatrees_tools.py
+++ AnaTreesPackage/mac/anatrees_tools.py
@@ -388,7 +388,7 @@ def extract_anatrees_tracks_information_with_all_features( in_chain, Option,
         do_continue = True
         calc.GetEntry( entry )
         entry_rse = [calc.run,calc.subrun,calc.event]
-        if flags.verbose>2: print entry_rse
+        if flags.verbose>3: print entry_rse
         
         if AddEventsList:
             
@@ -430,9 +430,9 @@ def extract_anatrees_tracks_information_with_all_features( in_chain, Option,
                     if "add hard geometrical cuts" in Option:
                         # sample of free protons candidates from off-beam data using only geometrical cuts
                         if flags.verbose>2:
-                            print 'track length: ',track.length
-                            print 'track position: (',track.start_pos.x(),track.start_pos.y(),track.start_pos.z(),',) => (', track.end_pos.x(),track.end_pos.y(),track.end_pos.z(),')'
-                            print 'track closest-flash time: ',track.cftime
+                            print 'track length: %.1f cm'%track.length
+                            print 'track position: (%.1f,%.1f,%.1f)=>(%.1f,%.1f,%.1f)'%(track.start_pos.x(),track.start_pos.y(),track.start_pos.z(), track.end_pos.x(),track.end_pos.y(),track.end_pos.z())
+                            print 'track closest-flash time: %.1f ns'%track.cftime
                         
                         # (1) track less then some minimum length 10 cm proton has ~ 800 MeV/c
                         length_cut = True if (track.length < 10) else False
