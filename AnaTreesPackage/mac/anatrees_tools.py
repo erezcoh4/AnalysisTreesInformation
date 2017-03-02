5f3b96afae53c03615e5e9bc4269b27e7f514843
Erez Cohen <cohen.erez7@gmail.com>
2017-03-02 13:16:27 -0600

    Thu Mar  2 13:16:27 CST 2017

--- AnaTreesPackage/mac/anatrees_tools.py
+++ AnaTreesPackage/mac/anatrees_tools.py
@@ -946,7 +946,6 @@ def extract_anatrees_information(in_chain=None, Option='', eventsTree=None, GENI
             if calc.mcevts_truth > 0: #{
                 calc.FillGENIETree()
             #}
-            print '\nfilled GENIE tree....\n'
         #} end geant4 particles
         
         
