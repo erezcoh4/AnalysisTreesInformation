694c26123dd2fb3e9e18b5a8b328d61d4a5b2498
Erez Cohen <cohen.erez7@gmail.com>
2017-03-03 16:27:00 -0600

    Fri Mar  3 16:27:00 CST 2017

--- AnaTreesPackage/mac/anatrees_tools.py
+++ AnaTreesPackage/mac/anatrees_tools.py
@@ -881,8 +881,12 @@ def extract_anatrees_tracks_information_from_files_list(data_type="BNB_5e19POT",
     i_file = 0
     for file in files: #{
         
+        if flags.verbose and i_file%(len(files)/10)==0: print'%.0f'%(100.*float(i_file)/len(files)) + '%'
+
+
         if debug: print_filename( file , "reading analysistree data from file (%.2f MB)"%filesize_in_MB(file) )
         if filesize_in_MB(file) < 0.1 : continue
+        
         
         in_chain = ROOT.TChain("analysistree/anatree")
         in_chain.Add( file )
@@ -894,6 +898,7 @@ def extract_anatrees_tracks_information_from_files_list(data_type="BNB_5e19POT",
                                      MCmode=MCmode, do_pandora_cosmic=do_pandora_cosmic )
                                      
         if debug: print_filename( file , "finished extracting anatrees information from file %d out of %d"%(i_file,len(files)-1) )
+        print_line()
         i_file += 1
     #}
     
@@ -966,9 +971,7 @@ def extract_anatrees_information(calc=None,#in_chain=None, Option='',i_file=0,# 
         
         calc.extract_information( True )
         
-        if flags.verbose and entry%flags.print_mod==0:
-            print'%.0f'%(100.*float(entry)/Nreduced) + '%'
-            if flags.verbose>1 and entry%flags.print_mod==0: calc.PrintData( entry )
+        if flags.verbose and entry%flags.print_mod==0: calc.PrintData( entry )
         
         if flags.verbose>2: print 'stream event features to file'
         stream_event_features_to_file ( calc , events_writer )
