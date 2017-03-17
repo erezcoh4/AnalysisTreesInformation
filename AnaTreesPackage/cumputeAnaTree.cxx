6032cd79bec43ce93687fa4181421761d9b14f8c
Erez Cohen <cohen.erez7@gmail.com>
2017-03-17 19:34:08 +0200

    Fri Mar 17 19:34:08 IST 2017

--- AnaTreesPackage/cumputeAnaTree.cxx
+++ AnaTreesPackage/cumputeAnaTree.cxx
@@ -283,6 +283,7 @@ void cumputeAnaTree::InitOutputTree(){
         GENIETree -> Branch("run"             ,&run               ,"run/I");
         GENIETree -> Branch("subrun"          ,&subrun            ,"subrun/I");
         GENIETree -> Branch("event"           ,&event             ,"event/I");
+        GENIETree -> Branch("hits"            ,&hits);
         // GENIETree -> Branch("pot"             ,&pot               ,"pot/D");
         GENIETree -> Branch("genie_interactions"  ,&genie_interactions); // genie interactions in a seperate tree...
     }
